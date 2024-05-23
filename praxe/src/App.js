030c7e255a548bdd4f7bd856a6eaf99537b776bf
Dejnyyy <danslaby@email.cz>
2024-05-23 12:24:21 +0200

    jo to pujde vyvarit cajl

--- praxe/src/App.js
+++ praxe/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [cars, setCars] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+  const [newCar, setNewCar] = useState({ name: '', brand: '', year: '', price: '' });
 
   useEffect(() => {
     axios.get('http://localhost:5001/api/cars')
@@ -20,7 +21,12 @@ function App() {
         setLoading(false);
       });
   }, []);
-  
+
+  const handleAddCar = () => {
+    setCars([...cars, newCar]);
+    setNewCar({ name: '', brand: '', year: '', price: '' });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,14 +39,42 @@ function App() {
           <ul>
             {cars.map((car, index) => (
               <li key={index}>
-                <h2 className=''>{car.name}</h2>
-                <p>Brand: {car.brand}</p>
+                <h2>{car.name}</h2>
+                <p>Brand: {car.model}</p>
+                <p>Type:{car.type}</p>
                 <p>Year: {car.year}</p>
-                <p>Price: ${car.price}</p>
+                <p>Availability: {car.is_available? "yes" : "no"}</p>
               </li>
             ))}
           </ul>
         )}
+        <div className="new-car-form">
+          <input
+            type="text"
+            placeholder="Name"
+            value={newCar.name}
+            onChange={(e) => setNewCar({ ...newCar, name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Brand"
+            value={newCar.brand}
+            onChange={(e) => setNewCar({ ...newCar, brand: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Year"
+            value={newCar.year}
+            onChange={(e) => setNewCar({ ...newCar, year: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Price"
+            value={newCar.price}
+            onChange={(e) => setNewCar({ ...newCar, price: e.target.value })}
+          />
+          <button onClick={handleAddCar}>+</button>
+        </div>
       </header>
     </div>
   );
